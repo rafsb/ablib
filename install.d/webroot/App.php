47b92242503bba9b2a17176a89720eedb5c6ff0c
rafsb <rafael@faau.me>
2019-08-21 15:22:55 -0300

    splash's restore button addede

--- install.d/webroot/App.php
+++ install.d/webroot/App.php
@@ -1,67 +1,53 @@
 <?php
-define("DEBUG",  true);
+define("DEBUG",  false);
 
-class App
-{
-    	private static $config = [
-        	"developer"                 => "DEV_team"
-	        , "project_name"            => "NEW_project"
-	        , "driver"                  => DISK
-	        , "get_config_min_level"    => MANAGER
-	        , "hash_algorithm"          => SHA512
-	        , "database_credentials"    => [
-			"host"      => "127.0.0.1"
-	                , "username"  => "root"
-            		, "passwd"    => ""
-		        	, "database"  => "test"
-            		, "encoding"  => "utf8"
-		]
-    	];
+class App {
+    private static $config = [
+        "devel"                     => "DEV Team"
+        , "project_name"            => "MobileApi"
+        , "driver"                  => DISK
+        , "get_config_min_level"    => MANAGER
+        , "hash_algorithm"          => SHA512
+        , "database_credentials"    => [
+            "host"      => "127.0.0.1"
+            , "username"  => "root"
+            , "passwd"    => "root"
+            , "database"  => "test"
+            , "encoding"  => "utf8"
+        ]
+    ];
 
-    	private static $datasources = [
+    private static $datasources = [
+        
+        "default"  => []
+        
+    ];
 
-        	"default"  => []
+    public static function connections($datasource=DEFAULT_DB){
+        $tmp = isset(self::$datasources[$datasource]) ? self::$datasources[$datasource] : [];
+        if(!isset($tmp["host"]))     $tmp["host"]     = self::config["database_credentials"][$host];
+        if(!isset($tmp["username"])) $tmp["username"] = self::config["database_credentials"][$username];
+        if(!isset($tmp["passwd"]))   $tmp["passwd"]   = self::config["database_credentials"][$passwd];
+        if(!isset($tmp["database"])) $tmp["database"] = self::config["database_credentials"][$database];
+        if(!isset($tmp["encoding"])) $tmp["encoding"] = self::config["database_credentials"][$encoding];
+        return $tmp;
+    }
 
-    	];
-
-    	public static function connections($datasource=DEFAULT_DB)
-	{
-        	$tmp = isset(self::$datasources[$datasource]) ? self::$datasources[$datasource] : [];
-
-        	if(!isset($tmp["host"]))     $tmp["host"]     = self::$config["database_credentials"][$host];
-        	if(!isset($tmp["username"])) $tmp["username"] = self::$config["database_credentials"][$username];
-        	if(!isset($tmp["passwd"]))   $tmp["passwd"]   = self::$config["database_credentials"][$passwd];
-        	if(!isset($tmp["database"])) $tmp["database"] = self::$config["database_credentials"][$database];
-        	if(!isset($tmp["encoding"])) $tmp["encoding"] = self::$config["database_credentials"][$encoding];
-
-		return $tmp;
-    	}
-
-    	public function driver($drv=null)
-	{
-        	return $drv ? ($drv==self::config("driver") ? true : false) : self::config("driver");
-    	}
-
-	public static  function config($field=null)
-	{
+    public function driver($drv=null){
+        return $drv ? ($drv==self::config("driver") ? true : false) : self::config("driver");
+    }
+    
+	public static  function config($field=null){
 		if($field && isset(self::$config[$field])) return self::$config[$field];
 		return User::level(self::$config["get_config_min_level"]) ? self::$config : null;
 	}
 
-	public static function devel()
-	{
-		return App::config("developer");
-	}
+	public static function devel(){ return App::config("developer"); }
 
-	public static function project_name()
-	{
-		return App::config("project_name");
-	}
+	public static function project_name(){ return App::config("project_name"); }
 
-	public static function init()
-	{
-		if(User::logged()) (new Dash)->render();
-		else (new Login)->render();
-	}
+	public static function init(){ 
+        if(!is_file(IO::root("var/hashes/cypher"))) IO::write("var/hashes/cypher",Hash::word());
+        (new Home)->render(); 
+    }
 }
-
