bb1b7d889874efef247d00263811e25426cd3963
rafsb <r_sb@live.com>
2020-04-18 17:00:54 -0300

    updating libs

--- js/tiles/tag.js
+++
@@ -1,8 +0,0 @@
-app.components.tag = _("div", "-left", {
-    fontSize:".75em"
-    , padding:".5em"
-    , borderRadius:".25em"
-    , background:app.colors("PUMPKIN")
-    , color: app.colors("CLOUDS")
-}).text("a tag")
-bootstrap.ready("tag")
