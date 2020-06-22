20cabfb437bf8c7d62ef39e0cdbe86be9d8c722f
rafsb <r_sb@live.com>
2020-06-22 13:20:09 -0300

    added vector extemded capabilities

--- php/Vector.class.php
+++ php/Vector.class.php
@@ -12,6 +12,11 @@ class Vector extends Activity {
 		return $return;
 	}
 
+	public static function iterate(float $beg, float $end, Closure $fn, float $step=null){
+		$step = $step ? $step : 1.0;
+		for(;$beg!=$end;$beg+=$step) $fn($beg);
+	}
+
 	public static function each(Array $arr, Closure $fn){
 		if(!empty($arr)) foreach($arr as $k=>&$v) $fn($v,$k);
 	}
@@ -81,6 +86,56 @@ class Vector extends Activity {
         $narr[] = $arr[sizeof($arr)-1];
         return $narr;
     
-    }	
+    }
+
+    public static function sum(Array $mix) {
+    	$x = 0;
+		self::each($mix, function($y) use (&$x){ $x+=$y; });
+		return $x;
+	}
+
+	public static function average(Array $mix) {
+		return self::sum($mix) / sizeof($mix);
+	}
+
+	public static function harmonic_average(Array $mix) {
+		$x = 0;
+		self::each($mix, function($y) use (&$x){ $x+=(1/$y); });
+		return sizeof($mix) / $x;
+	}
+
+	public static function linear_trend(Array $mix, Number $h=null) {
+        $np = sizeof($mix);
+        $m = $b = $x = $y = $x2 = $xy = 0;
+        if($h===null) $h = $np;
+        self::each($mix, function($n, $i) use (&$x, &$y, &$xy, &$x2){
+            $x = $x + $i;
+            $y = $y + $n;
+            $xy = $xy + $i * $n;
+            $x2 = $x2 + $i * $i;
+        });
+        $z = $np * $x2 - $x * $x;
+        if($z){
+            $m = ($np * $xy - $x * $y) / $z;
+            $b = ($y * $x2 - $x * $xy) / $z;
+        }
+        return $m * $h + $b;
+	}
+
+	public static function interpolate(Array $mix, float $x=null) {
+		if($x===null) $x = count($mix)+1;
+        $yi = $mix;
+        $xi = array_keys($mix);
+        $n  = count($mix);
+        $sum = 0;
+        self::iterate(0, $n, function($k) use ($x, $xi, $yi, $n, &$sum){
+        	$prod = 1;
+        	Vector::iterate(0, $n, function($i) use ($x, $xi, $k, &$prod){
+        		if ($i!=$k) $prod = $prod * ($x - $xi[$i]) / ($xi[$k] - $xi[$i]);
+        	});
+            $sum += $yi[$k] * $prod;
+        });
+        return $sum;
+	}
 
 }
