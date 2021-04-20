697839c2cfb1ce21a70048f0ffee268a024f3812
rafsb <r_sb@live.com>
2021-04-20 14:52:45 -0300

    some defs

--- install.d/webroot/config.js
+++ install.d/webroot/config.js
@@ -1,60 +1,63 @@
 
 const
+/*
+ *
+ * CONFIG FILE
+ *
+ */
+START 				= 0
+, ANIMATION_LENGTH 	= 400
+, AL 				= ANIMATION_LENGTH
+
+, API_PROTOCOL 		= "https://"
+, API_PREFIX 		= ""
+, API_HOST 			= "localhost"
+, API_SUFIX 		= ":6443/"
+
+, APP_DEFAULT_THEME = "light"
+, APP_NEEDS_LOGIN = false
+
+, __Swipe__ 		= new Swipe()
+, __come__ 			= new Event('come')
+, __go__ 			= new Event('go')
+
+/*
+ * USER LEVELS
+ */
+, EEvents = {
+	CLICK: 		  	"click"
+	, MOUSEENTER: 	"mouseenter"
+	, MOUSELEAVE: 	"mouseleave"
+	, SUBMIT: 	  	"submit"
+}
+, EUsers = {
+	LOGGED: 		0
+	, USER: 		1
+	, EDITOR: 		2
+	, MANAGER: 		3
+	, TI: 			4
+	, DIRECTOR: 	5
+	, ADMIN: 		6
+	, DEV: 			7
+	, SYSTEM: 		8
+	, ROOT: 		9
+}
 	/*
-	 *
-	 * CONFIG FILE
-	 *
-	 */
-	ANIMATION_LENGTH = 400
-	, AL = ANIMATION_LENGTH
+ *
+ * CLIENT PREDEFINED CONSTANTS
+ *
+ ***********************************************************************/
 
-	, API_PROTOCOL = "https://"
-	, API_PREFIX = ""
-	, API_HOST = "localhost"
-	, API_SUFIX = ":443/"
-	, API_HOSTNAME = API_PROTOCOL + API_PREFIX + API_HOST + API_SUFIX
 
-	, APP_DEFAULT_THEME = "light"
-	, APP_NEEDS_LOGIN = false
 
-	/*
-	 * USER LEVELS
-	 */
-	, EUsers = {
-		LOGGED: 	0
-		, USER: 	1
-		, EDITOR: 	2
-		, MANAGER: 	3
-		, TI: 		4
-		, DIRECTOR:	5
-		, ADMIN: 	6
-		, DEV: 		7
-		, SYSTEM: 	8
-		, ROOT: 	9
-	}
 
-	/*
-	 * EVENTS
-	 */
-	, EEvents = {
-		CLICK: 			"click"
-		, MOUSEENTER:	"mouseenter"
-		, MOUSELEAVE: 	"mouseleave"
-		, SUBMIT: 		"submit"
-	}
+
+
+
+
 	
-	/*
-	 * PARADIGMS
-	 */
-	EPragmas = {
-		START: 		0
-		, MENU: 	1
-		, RELOAD: 	2
-	}
-
-	/*
-	 *
-	 * CLIENT PREDEFINED CONSTANTS
-	 *
-	 */
+/***********************************************************************
+ * ROUTER HOST NAME
+ */
+, API_HOSTNAME = API_PROTOCOL + API_PREFIX + API_HOST + API_SUFIX
 ;;
